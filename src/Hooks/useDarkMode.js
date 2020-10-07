5bf93e0c7f915112dafbea81c6f251dc75e6b2fb
Blue-Ranger <53351056+Blue-Ranger@users.noreply.github.com>
2020-10-06 20:32:00 -0400

    solution code added

--- src/Hooks/useDarkMode.js
+++ src/Hooks/useDarkMode.js
@@ -1,17 +1,15 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
+import { useLocalStorage } from "./useLocalStorage";
 
+export const useDarkMode = () => {
+  const [darkMode, setDarkMode] = useLocalStorage("darkMode", false);
 
-const useDarkMode = () => {
-  const [darkMode, setDarkMode] = useState()
+  useEffect(() => {
+    const body = document.querySelector("body");
+    darkMode
+      ? body.classList.add("dark-mode")
+      : body.classList.remove("dark-mode");
+  }, [darkMode]);
 
-useEffect () => {
-
-}
-
-
-
-
-
-
+  return [darkMode, setDarkMode];
 };
-export default useDarkMode;
--- src/Hooks/useLocalStorage.js
+++ src/Hooks/useLocalStorage.js
@@ -1,5 +1,15 @@
 import { useState } from "react";
 
-const useLocalStorage = () => {};
+export const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
 
-export default useLocalStorage;
+  const setValue = (value) => {
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  return [storedValue, setValue];
+};
